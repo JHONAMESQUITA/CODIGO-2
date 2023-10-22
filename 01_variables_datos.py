004198b88ade77fa64d27d3c91f9a0d02dca791e
JHONAMESQUITA <146906590+JHONAMESQUITA@users.noreply.github.com>
2023-10-22 12:48:04 -0500

    Add files via upload

---
+++ 01_variables_datos.py
@@ -0,0 +1,26 @@
+# Declaración de variables con diferentes tipos de datos
+nombre = "Juan El Caballo"          
+
+edad = 19                
+
+altura_metros = 1.93            
+
+es_estudiante = True     
+
+print("edad de", nombre, "es", edad)
+
+
+# tipo de variable segun el dato
+print("Variable 'nombre' es de tipo:", type(nombre))
+print("Variable 'edad' es de tipo:", type(edad))
+print("Variable 'altura' es de tipo:", type(altura_metros))
+print("Variable 'es_estudiante' es de tipo:", type(es_estudiante))
+
+# operaciones 
+edad_doble = edad * 2
+altura_centimetro = altura_metros * 100
+
+#los resultados de las operaciones
+print("El doble de la edad es:", edad_doble )
+print("La altura en centímetros es:", altura_centimetro)
+
---
+++ 04.py
@@ -0,0 +1,13 @@
+# primer programa que escribes en un nuevo lenguaje
+from importlib import import_module
+from pickletools import int4
+from re import A
+#FICHA 2
+
+
+
+v=float(input("valor de el voltaje "))
+
+r=float(input("valor de la resistencia "))
+i=v/r
+print ("la corriente es",i)
